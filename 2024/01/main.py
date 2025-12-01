f4ccdf334195900348bbc64ab8ffca16b86786ae
sahana <shayaandoodekula950@gmail.com>
2025-11-30 23:30:43 -0500

    uwu

--- 2024/01/main.py
+++ 2024/01/main.py
@@ -3,5 +3,7 @@ import sys; args = sys.argv[1:]
 list = open(args[0]).read().split()
 leftList = sorted([int(i) for i in list[::2]])
 rightList = sorted([int(i) for i in list[1::2]])
-sum = sum(abs(leftList[i] - rightList[i]) for i in range(len(leftList)))
+
+sum = sum(leftList[i] * rightList.count(leftList[i]) for i in range(len(leftList)))
+
 print(sum)
