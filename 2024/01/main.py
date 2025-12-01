5da8b423d68c77d9cd23ec900148ba38d7c425fb
sahana <shayaandoodekula950@gmail.com>
2025-11-30 23:31:08 -0500

    ea

--- 2024/01/main.py
+++ 2024/01/main.py
@@ -4,6 +4,6 @@ list = open(args[0]).read().split()
 leftList = sorted([int(i) for i in list[::2]])
 rightList = sorted([int(i) for i in list[1::2]])
 
-sum = sum(leftList[i] * rightList.count(leftList[i]) for i in range(len(leftList)))
-
-print(sum)
+print(
+    sum(leftList[i] * rightList.count(leftList[i]) for i in range(len(leftList)))
+)
