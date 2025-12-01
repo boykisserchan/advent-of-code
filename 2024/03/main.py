b79c5e6fa61f04f89be4ed30777f74eaba92eaa2
sahana <shayaandoodekula950@gmail.com>
2025-12-01 12:16:56 -0500

    uwu

--- 2024/03/main.py
+++ 2024/03/main.py
@@ -2,6 +2,15 @@ import re
 import sys; args = sys.argv[1:]
 
 instructions = open(args[0]).readlines()
+sum = 0
+enabled = True
 for inst in instructions:
-    for i in re.match(r"mul(\d{1-3},\d{1-3})"):
-        x, y = re.match("mul\((\d{1,3}),(\d{1,3})\)", i).groups()
+    for i in re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", inst):
+        if i == "do()":
+            enabled = True
+        elif i == "don't()":
+            enabled = False
+        elif enabled:
+            x, y = re.match(r"mul\((\d{1,3}),(\d{1,3})\)", i).groups()
+            sum += int(x) * int(y)
+print(sum)
--- 2025/01/main.py
+++ 2025/01/main.py
@@ -1,13 +1,13 @@
 import sys; args = sys.argv[1:]
 
-list = open(args[0]).read().split()
+list = open(args[0]).readlines()
 it = [(n[0], int(n[1:])) for n in list]
 count = 0
 dial = 50
 for d, c in it:
     polarity = 1
     if d == "L":
-        polarity *= -1
+        polarity = -1
     for _ in range(c):
         dial += polarity
         dial = dial % 100
