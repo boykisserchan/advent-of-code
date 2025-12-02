8787e33c9b62bf38a10111325e2cb19f7de51c1e
sahana <shayaandoodekula950@gmail.com>
2025-12-02 00:11:16 -0500

    day 2 wooo

--- 2025/02/main.py
+++ 2025/02/main.py
@@ -1,4 +1,13 @@
+import re
 import sys; args = sys.argv[1:]
 
-list = open(args[0]).readlines()
-
+list = open(args[0]).read()
+ranges = list.split(",")
+ids = [map(int, i.split("-")) for i in ranges]
+sum = 0
+pattern = r"^(\d+?)\1+$"
+for l, r in ids:
+    for id in range(l, r):
+        if re.match(pattern, str(id)):
+            sum += id
+print(sum)
